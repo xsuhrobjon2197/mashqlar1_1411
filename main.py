fae9881cc9358e691580c18de1759727de9989e4
xsuhrobjon2197 <xsuhrobjon2197@gmail.com>
2025-11-14 15:26:54 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+#1-misol
+i = "   Python dasturlash   "
+print(len(i))
+x = i.lstrip()
+print(x)
+
+#2-misol
+i = 'hello world'
+x = i.upper()
+print(x)
+print(len(i))
+
+#3-misol
+i = "   SALOM DUNYO 123    "
+x = i.lower()
+c = i.strip()
+print(x)
+print(c)
+
+#4-misol
+i = "   o'zbekiston    "
+x = i.rstrip()
+print(x)
+
+#5-misol
+i = "python3.11"
+x = i.title()
+print(x)
+print(len(i))
+
+#6-misol
+i = "   Toshkent 2025"
+x = i.strip()
+print(x)
+c = i.upper()
+print(c)
+
+#7-misol
+i = "ALGARITM VA DASTURLASH"
+x = i.lower()
+print(x)
+
+#8-misol
+i = "   java vs python      "
+x = i.title()
+print(x)
+c = i.split()
+print(c)
